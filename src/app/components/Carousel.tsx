6edc682e9d7e619eac0e01d79324c710438f5885
Joyce <joyce.mbt@gmail.com>
2024-03-16 14:31:15 -0300

    updated speakers

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
+"use client";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
 interface CarouselProps {
@@ -11,33 +11,56 @@ const Carousel: React.FC<CarouselProps> = ({ speakers }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === speakers.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === speakers.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? speakers.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? speakers.length - 1 : prevIndex - 1
+    );
   };
 
   return (
-    <>
-      <h1 className="text-3xl font-bold pb-4 pt-4">Palestrantes</h1>
-      <div className="carousel flex items-center">
+    <div className="py-5 flex flex-col gap-2 items-center">
+      <h1 className="text-3xl font-bold pb-4 pt-4 text-gray-300">
+        Palestrantes
+      </h1>
+      <div className="carousel flex items-center text-gray-300">
         <button onClick={prevSlide}>
-        <Image src="/previous.png" alt="Carousel Previous Icon" className="" width={50} height={50} />
+          <Image
+            src="/previous.svg"
+            alt="Carousel Previous Icon"
+            className=""
+            width={50}
+            height={50}
+          />
         </button>
         <div className="flex flex-col md:flex-row justify-center mb-4">
-          <img src={speakers[currentIndex].url} alt={`Slide ${currentIndex + 1}`} className="rounded-full h-72 w-72 object-cover" />
-          <div className="flex flex-col justify-center pl-6 pb-6">
+          <img
+            src={speakers[currentIndex].url}
+            alt={`Slide ${currentIndex + 1}`}
+            className=" h-72 w-72 object-cover"
+          />
+          <div className="flex flex-col justify-center pl-6 pb-6 gap-2">
             <h2 className="text-lg font-bold">{speakers[currentIndex].name}</h2>
-            <p className="text-md max-w-96">{speakers[currentIndex].description}</p>
+            <p className="text-md max-w-96">
+              {speakers[currentIndex].description}
+            </p>
           </div>
-
         </div>
         <button onClick={nextSlide}>
-        <Image src="/next.png" alt="Carousel Next Icon" className="" width={50} height={50} />
+          <Image
+            src="/next.svg"
+            alt="Carousel Next Icon"
+            className=""
+            width={50}
+            height={50}
+          />
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
